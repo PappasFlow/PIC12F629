52be9facd1273534e50c9d9a4213e0b10868035a
joaquin pappano <114824831+PappasFlow@users.noreply.github.com>
2024-04-18 12:17:15 -0300

    buzzer y IO

--- funtions.c
+++ funtions.c
@@ -1,15 +1,22 @@
 #include "header.h"
 
+
+void init (void){
+    TRISIO = TRI;
+    GPIO = 0; //inicia todos los gpio a cero
+}
+
+
 //hace parpadear el led cada ves que state crese
 void blink  (void){     
     static unsigned int count = 0;
     if (count > state)
         count++;
     else
-        digitalwrite(LED,LOW);
+        LED=LOW;
     
     if(count < state){
-        digitalwrite(LED,HIGH);
+        LED=HIGH;
         count = state + 1;}
     
     if (count >= TIME_BLINK)
@@ -18,16 +25,16 @@ void blink  (void){
 
 void button  (void){
     static unsigned int count = 0;
-    if(count==0 && state==ESPERA && digitalread(BOTON)){ //inicia cuenta si esta en inicio
+    if(count==0 && state==ESPERA && (BOTON==1) ){ //inicia cuenta si esta en inicio
         //state=F1K; //opcional inicia al momento del click en el estado F1K
         count=1;}
     
-    if(count==0 && (state!=ESPERA) && digitalread(BOTON)){ //reinicia estado 0 si se preciona en funcionamiento
-        digitalwrite(BUZZ,LOW); //apaga el buzzer
-        while(digitalread(BOTON)){}
+    if(count==0 && (state!=ESPERA) && (BOTON==1) ){ //reinicia estado 0 si se preciona en funcionamiento
+        BUZZ==LOW; //apaga el buzzer
+        while( (BOTON==1) ){}
         state=ESPERA;}
     
-    if((count!=0) && digitalread(BOTON)) //cuenta mientras se precione o reinicia al soltar
+    if((count!=0) && (BOTON==1) ) //cuenta mientras se precione o reinicia al soltar
         count++;
     else
        count=0; 
@@ -37,21 +44,6 @@ void button  (void){
         count=1;}
         
 }
-void controlBuzz(void){
-    char frec[6]={0,16,8,4,2,1};
-    static char toogle =0;
-    static char contador =0;
-    contador =+1; 
-    
-        if(contador >= frec[state]){
-        toogle= !toogle;
-        GPIO5 =(__bit)toogle;
-        contador=0;
-        }else if(state==0){
-        GPIO5 =0;
-        contador=0;
-       }
-}
 
 
 // cuenta un tiempo desde que se suelta el boton y si se cumple vuelve a estado espera
@@ -60,6 +52,20 @@ void one_minute (void){
     count++;
     if (count >= TIME_MINUTE)
         state=ESPERA;
-    if ( digitalread(BOTON) )
+    if ( BOTON==1 )
         count=0;
+}
+
+
+void controlBuzz(void){
+    unsigned char frec[6]={0,16,8,4,2,1};
+    static unsigned int contador =0;
+    contador++; 
+    
+        if(contador >= (frec[state]*TIME_FREC) && (state!=ESPERA) ){
+            BUZZ= !(BUZZ==1); //toogle
+            contador=0;
+        }else if(state==ESPERA){
+            BUZZ =0;
+       }
 }
