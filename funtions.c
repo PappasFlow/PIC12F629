94ff7751ca9b095e660f77cfcde0f68c64e64115
joaquin pappano <114824831+PappasFlow@users.noreply.github.com>
2024-04-20 16:48:08 -0300

    v4

--- funtions.c
+++ funtions.c
@@ -4,112 +4,64 @@
 void init (void){
     TRISIO = TRI;
     GPIO = 0; //inicia todos los gpio a cero
-    GPIO0 =1;
+    TX = HIGH;
+    
     T0CS = 0;         // Timer0 Clock Source Select bit: Internal instruction cycle clock (CLKOUT)
     PSA = 0;          // Prescaler Assignment bit: Prescaler is assigned to the Timer0 module
     PS2 =1;          // Prescaler Rate Select bits: 1:128 Prescaler value
     PS1 = 1;
     PS0 = 1;
-    
     // Calcular el valor de TMR0 para una interrupción cada 32 kHz
     TMR0 =0;// Prescaler 1:128, Frecuencia deseada = 32000 Hz (32 kHz)
-    
     T0IE = 1;         // Enable Timer0 overflow interrupt
-    GIE = 0;          // Enable Global Interrupt Enable bit
+    
+    GIE = 1;          // Enable Global Interrupt Enable bit
+    
 }
 
-union CharBits {
-    char byte;
-    struct {
-        unsigned char bit0 : 1;
-        unsigned char bit1 : 1;
-        unsigned char bit2 : 1;
-        unsigned char bit3 : 1;
-        unsigned char bit4 : 1;
-        unsigned char bit5 : 1;
-        unsigned char bit6 : 1;
-        unsigned char bit7 : 1;
-    };
-};
-union CharBits datosend[6];
-union CharBits dato;
-void carga(void){
-    char aux = '0';
-    for(char i=0;i<6;i++){
-        datosend[i].byte = aux + i;
-    }
+
+//hace parpadear el led cada ves que state crese
+void blink  (void){     
+    static unsigned char sp = ESPERA;
+    if (sp != state){
+        sp=state;
+        LED=HIGH;}
+    else
+        LED=LOW;
 }
 
-void send(void){
-     GPIO0 =LOW;
-     __delay_us(100);
-     GPIO0  =(__bit)datosend[state].bit0;
-     __delay_us(90);
-     GPIO0 =(__bit)datosend[state].bit1;
-     __delay_us(90);
-    GPIO0  =(__bit)datosend[state].bit2;
-     __delay_us(90);
-     GPIO0  =(__bit)datosend[state].bit3;
-     __delay_us(90);
-     GPIO0 =(__bit)datosend[state].bit4;
-     __delay_us(90);
-     GPIO0  =(__bit)datosend[state].bit5;
-     __delay_us(90);
-     GPIO0  =(__bit)datosend[state].bit6;
-     __delay_us(90);
-     GPIO0 =(__bit)datosend[state].bit7;
-     __delay_us(90);
-     GPIO0  =HIGH;
-}
-void recieve(void){
-    __delay_us(90);
-    dato.bit0= GPIO1;
-    __delay_us(100);
-    dato.bit1= GPIO1;
-    __delay_us(100);
-    dato.bit2= GPIO1;
-    __delay_us(100);
-    dato.bit3= GPIO1;
-    __delay_us(100);
-    dato.bit4=GPIO1;
-    __delay_us(100);
-    dato.bit5= GPIO1;
-    __delay_us(100);
-    dato.bit6= GPIO1;
-    __delay_us(100);
-    dato.bit7= GPIO1;
-    if(dato.byte == 0x1b)
-        state = 0;
-}
 void button  (void){
     static unsigned int count = 0;
-    static unsigned int flag = 0;
-    if(count==0 && state==ESPERA && (BOTON==1) ){ //inicia cuenta si esta en inicio
-        //state=F1K; //opcional inicia al momento del click en el estado F1K
-        count=1;}
     
-    if(count==0 && (state!=ESPERA) && (BOTON==1) ){ //reinicia estado 0 si se preciona en funcionamiento
-        state=ESPERA;}
+    if(BOTON==1){
+        __delay_us(20);//evita efecto rebote
     
-    if((count!=0) && (BOTON==1) ) //cuenta mientras se precione o reinicia al soltar
-        count++;
-    else{
-       count=0; 
-       flag =1;
-    }
-    if(flag){
-        (count==60) ? state=0 : count++;
-    }
-if ((count >= state) && !flag) {
-    if(state >= 5)
-    state = 1;
-    else
-    state = (char)count;
-    GPIO4 = 1;
-    count = 1;
-}}
-void one_Second(void){
-    button(); 
-    send();
-    GPIO4=0;
+        if(count==0 && state==ESPERA && (BOTON==1) ){ //inicia cuenta si esta en inicio
+            state=F1K; //opcional inicia al momento del click en el estado F1K
+            count=1;}
+
+        if(count==0 && (state!=ESPERA) && (BOTON==1) ){ //reinicia estado 0 si se preciona en funcionamiento
+            BUZZ=LOW; //apaga el buzzer
+            while( BOTON==1 ){}
+                state=ESPERA;}
+
+        if((count!=0) && (BOTON==1) ) //cuenta mientras se precione
+            count++;
+
+        if(count >= TIME_BUTTON){ //aumenta el estado
+            (state>F16K) ? state=F1K : state++;
+            count=1;}
+    } else
+        count=0;  //reinicia al soltar
+}
+
+
+// cuenta un tiempo desde que se suelta el boton y si se cumple vuelve a estado espera
+void one_minute (void){
+    static unsigned int count = 0;
+    count++;
+    if (count >= TIME_MINUTE)
+        state=ESPERA;
+    if ( BOTON==1 )
+        count=0;
 }
