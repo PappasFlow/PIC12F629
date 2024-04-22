f9f445f023f5bf25cff024104864c2943b87a6e2
joaquin pappano <114824831+PappasFlow@users.noreply.github.com>
2024-04-22 15:28:15 -0300

    v5

--- main.c
+++ main.c
@@ -2,9 +2,14 @@
 
 
 void __interrupt() ISR() { 
+    if(GPIF && (RX==0) ){
+        //recieve();  //no aplica sobremuestreo
+        recieve_os(); //aplica sobremuestreo
+        GPIF=0;
+    }
     if (T0IF) { 
          static char contador = 0;
-         blink();
+         //blink();
          if(contador  >= 16){//entra cada 1 seg
              TX_frec();
              contador=0;
@@ -15,37 +20,12 @@ void __interrupt() ISR() {
          TMR0 = 0;
          T0IF=0;
     }
-    
+    loop ();
 }
 
 void main(void) {
     init();
     while(1){
-         switch(state) {
-            case ESPERA:
-                BUZZ = 0;
-                break;
-            case F1K:
-                BUZZ ^= 1;
-                __delay_us(TIME_FREC*16);
-                break;
-            case F2K:
-                BUZZ ^= 1;
-                __delay_us(TIME_FREC*8);
-            break;
-            case F4K:
-                BUZZ ^= 1;
-                __delay_us(TIME_FREC*4);
-               break;
-            case F8K:
-                BUZZ ^= 1;
-                __delay_us(TIME_FREC*2);
-                break;
-             case F16K:
-                BUZZ ^= 1;
-                __delay_us(TIME_FREC);
-                break;
-             default:state=ESPERA;}
-    } //fin superbucle  
-    
+        loop ();
+    } //fin superbucle     
 }
