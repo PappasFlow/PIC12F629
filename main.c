3026840d14f3c02bd7ec7cbcb81405bb74991d33
joaquin pappano <114824831+PappasFlow@users.noreply.github.com>
2024-04-23 22:08:04 -0300

    v6

--- main.c
+++ main.c
@@ -4,21 +4,17 @@ char segMas1 = 0;
 char flag = 0;
 void __interrupt() ISR() { 
     if(GPIF && (RX==0) ){
-        
-        //recieve();  //no aplica sobremuestreo
-        recieve_os(); //aplica sobremuestreo
         GPIF=0;
         IOC2 = 0;
         GIE=0;
         segMas1=1;
-        //__delay_us(100);
-
         flag = 1;
-    }
-    if (T0IF) { 
+        
+        //recieve();  //no aplica sobremuestreo
+        recieve_os(); //aplica sobremuestreo
+    }else if (T0IF) { 
          static char contador = 0;
-         //blink();
-         if(contador  >= 16){//entra cada 1 seg
+         if(contador  >= TIME_TOIF){//entra cada 1 seg
              segMas1 = 1;
              contador=0;
          } else 
@@ -26,18 +22,16 @@ void __interrupt() ISR() {
          TMR0 = 0;
          T0IF=0;
     }
-    //loop ();
 }
 
 void main(void) {
     init();
-    LED=1;
     while(1){
         if(segMas1 ==1 ){
              TX_frec();
-             button();  
+             button();
+             blink();
              one_minute();
-             //blink();
              segMas1 = 0;
         }
         if(flag ==1 ){
@@ -50,3 +44,4 @@ void main(void) {
         
     }
 }
+
